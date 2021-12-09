f6d976d0c70571a3dea56091246fa3888fb68c51
BenBurchfield <BenBurchfield@gmail.com>
2021-12-08 19:25:16 -0600

    Updates from08Dec

--- php/config.php
+++ php/config.php
@@ -9,9 +9,75 @@
 
     $conScouting = new mysqli($host_name, $user_name, $password, $database);
 
-    echo 'This is an echo';
+    function newTeam($TeamName,$StreetAddress,$ZIP,$State,$ContactNumber,$Owner,$FirstName,$LastName,$Phone,$Password){
+        $strErrorMessage = '';
+        $blnError = false;
+        if(strlen($TeamName) < 1 || $TeamName == null){
+            $strErrorMessage = $strErrorMessage . 'TeamName must be passed to web service | ';
+            $blnError = true;
+        }
+        if(strlen($StreetAddress) < 1 || $StreetAddress == null){
+            $strErrorMessage = $strErrorMessage . 'Street Address must be passed to web service | ';
+            $blnError = true;
+        }
 
-    function newTeam($strTeamName,$strStreetAddress,$strZIP,$strState,$strContactNumber,$strOwnerEmail,$strFirstName,$strLastName,$strPhone,$strPassword) {
-        echo 'This is a test';
+        // Lindsey add the other checks here
+
+        if($blnError == true){
+            echo '{"Outcome":"Error:'.$strErrorMessage.'"}';
+            return '{"Outcome":"Error:'.$strErrorMessage.'"}';
+        } else {
+            global $conScouting;
+            $strTeamID = guidv4();
+            $strStatus = 'ACTIVE';
+            $strQuery = "INSERT INTO tblTeams VALUES (?,?,?,?,?,?,?,?,?,(SELECT TeamKey FROM tblTeamKeys WHERE TeamKey NOT IN (SELECT b.TeamKey FROM tblTeams b) LIMIT 1))";
+              // Check Connection
+            try {
+                if ($conScouting->connect_errno) {
+                    $blnError = "true";
+                    $strErrorMessage = $conScouting->connect_error;
+                    $arrError = array('error' => $strErrorMessage);
+                    echo json_encode($arrError);
+                    exit();
+                }
+              
+                if ($conScouting->ping()) {
+                } else {
+                    $blnError = "true";
+                    $strErrorMessage = $conScouting->error;
+                    $arrError = array('error' => $strErrorMessage);
+                    echo json_encode($arrError);
+                }
+              
+                 $statCustodial = $conScouting->prepare($strQuery);
+        
+                 // Bind Parameters
+                 $statCustodial->bind_param('sssssssss', $strTeamID, $TeamName, $StreetAddress, $ZIP, $State, $ContactNumber, $Owner, $strStatus,$APIKey);
+                 
+                 if($statCustodial->execute()){
+                    if(newUser($Owner,$FirstName,$LastName,$Phone,$strTeamID,$Password) == '{"Outcome":"New User Created"}'){
+                        return '{"Outcome":"'.$strTeamID.'"}';
+                    } else {
+                        return '{"Outcome":"Error"}';
+                    }
+                    
+                 } else {
+                    return '{"Outcome":"Error"}';
+                 }
+        
+                 // $result = $statCustodial->get_result();
+                 
+                 // echo json_encode(($result->fetch_assoc()));
+                 $statCustodial->close();
+            }
+            catch (exception $ex) {
+                var_dump($ex);
+            }
+        }
+        
+        
     }
+
+    newTeam('','','','','','','','','','');
+
 ?>
