cf83fe3d417a6463a3900fe4d295ffde7f90ab07
lindseyfiscor <61033724+lindseyfiscor@users.noreply.github.com>
2021-12-11 10:47:52 -0500

    Added checks in config and changed vars in newteam

--- php/config.php
+++ php/config.php
@@ -9,19 +9,58 @@
 
     $conScouting = new mysqli($host_name, $user_name, $password, $database);
 
-    function newTeam($TeamName,$StreetAddress,$ZIP,$State,$ContactNumber,$Owner,$FirstName,$LastName,$Phone,$Password){
+    function newTeam($TeamName,$TeamNumber$StreetAddress,$ZIP,$State,$ContactNumber,$FirstName,$LastName,$Email){
         $strErrorMessage = '';
         $blnError = false;
+        $TeamName = trim($TeamName);
+        $TeamNumber= trim($TeamNumber);
+        $StreetAddress = trim($StreetAdress);
+        $ZIP = trim($ZIP);
+        $State = trim($State);
+        $ContactNumber = trim($ContactNumber);
+        $FirstName = trim($FirstName);
+        $LastName = trim($LastName);
+        $Email = trim($Email);
         if(strlen($TeamName) < 1 || $TeamName == null){
             $strErrorMessage = $strErrorMessage . 'TeamName must be passed to web service | ';
+            $blnError = true;
+        }
+        if(strlen($TeamNumber) < 1 || $TeamNumber == null || strlen($TeamNumber) > 4){
+            $strErrorMessage = $strErrorMessage . 'TeamNumber must be passed to web service | ';
             $blnError = true;
         }
         if(strlen($StreetAddress) < 1 || $StreetAddress == null){
             $strErrorMessage = $strErrorMessage . 'Street Address must be passed to web service | ';
             $blnError = true;
         }
-
-        // Lindsey add the other checks here
+        if(strlen($ZIP) < 1|| $ZIP == null || strlen($ZIP) != 6){
+            $strErrorMessage = $strErrorMessage . 'ZIP Code must be passed to web service | ';
+            $blnError = true;
+            //Do I need to check if have alpha characters?
+        }
+        if(strlen($State) < 1 || $State == null || strlen($State) > 2){
+            $strErrorMessage = $strErrorMessage . 'State must be passed to web service and must be two digits long | ';
+            $blnError = true;
+        }
+        if(strlen($ContactNumber) < 1 || $ContactNumber == null){
+            $strErrorMessage = $strErrorMessage . 'Phone Number must be passed to web service with area code | ';
+            $blnError = true;
+            // What checks should I add here?
+        }
+        if(strlen($FirstName) < 1 || $FirstName == null){
+            $strErrorMessage = $strErrorMessage . 'First Name must be passed to web service | ';
+            $blnError = true;
+        }
+        if(strlen($LastName) < 1 || $LastName == null){
+            $strErrorMessage = $strErrorMessage . 'Last Name must be passed to web service | ';
+            $blnError = true;
+        }
+        if(strlen($Email) < 1 || $Email == null || ){
+            $strErrorMessage = $strErrorMessage . 'Last Name must be passed to web service | ';
+            $blnError = true;
+            //need to parse the email and check the different parts
+        }
+        
 
         if($blnError == true){
             echo '{"Outcome":"Error:'.$strErrorMessage.'"}';
