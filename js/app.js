d302c7a1e0f7323d3b39327982d333fc6be830b8
BenBurchfield <BenBurchfield@gmail.com>
2022-02-01 18:50:04 -0600

    Sign in and redirect

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ $(document).on('click','#btnLogout', function() {
         showCancelButton: true,
         confirmButtonText: 'Yes'
     }).then((result) =>{
+        sessionStorage.removeItem('ScoutFRCSessionID');
         if(result.isConfirmed){
             window.location.href ="login.html";
         }
@@ -82,3 +83,44 @@ $(document).on('click','#btnLogin', function() {
 
     });
 });
+
+$(document).on('click','#btnSignIn',function(){
+    var objNewSessionResponse;
+    let blnError = false;
+    let strErrorMessage = '';
+    if(!$('#txtEmail').val()){
+        blnError = true;
+        strErrorMessage += '<p>Email/Username is Blank</p>';
+    }
+    if(!$('#txtPassword').val()){
+        blnError = true;
+        strErrorMessage += '<p>Password is Blank</p>';
+    }
+    if(blnError == true){
+        Swal.fire({
+            icon: 'error',
+            title: 'Missing Data',
+            html: strErrorMessage
+        })
+    } else {
+        var objNewSessionPromise = $.post('../php/newSession.php', { strUsername:$('#txtEmail').val(), strPassword: $('#txtPassword').val() }, function(result){
+            objNewSessionResponse = JSON.parse(result);
+        })
+    }
+    $.when(objNewSessionPromise).done(function() {
+        if(objNewSessionResponse.Outcome == 'Login Failed'){
+            Swal.fire({
+                icon: 'error',
+                title: 'Login Failed',
+                html: '<h3>Please review your username and password</h3>'
+            }) 
+        } else {
+            sessionStorage.setItem('ScoutFRCSessionID',objNewSessionResponse.Outcome);
+            Swal.fire({
+                icon: 'success',
+                title: 'Login Complete',
+                html: '<h3>Great Job!</h3>'
+            })
+        }
+    })
+})
